85ff50ead599f37a7401bc21b6d2504405328d2f
lirre0xF <liab8575@win.dsv.su.se>
2016-04-13 19:03:22 +0200

    Test2
    Test2

---
+++ Frame.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Frame {
+
+	
+	public Frame ()
+	{
+		JFrame main = new JFrame();
+		main.setVisible(true);
+		main.setSize(500, 350);
+		
+		JPanel textPanel = new JPanel();
+	}
+	
+	
+}
